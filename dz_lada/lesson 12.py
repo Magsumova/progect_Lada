c3488ba8ef48182d4b5f83bd5e29602f9f038e67
Lada <lada2179@mail.ru>
2024-01-18 00:16:19 +0600

    home_work

---
+++ dz_lada/lesson 12.py
@@ -0,0 +1,19 @@
+"""import math_operations
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+ans = input("Какую операцию хатите выполнить? ")
+
+if ans == "сложение":
+    summary = math_operations.summ(a,b)
+    print("Сумма", summary)
+elif ans == "разность":
+    raznost=math_operations.raz(a,b)
+    print("Разность", raznost)
+elif ans == "умножение":
+    umnojenie=math_operations.umnoj(a,b)
+    print("Умножение", umnojenie)
+else:
+    delenie=math_operations.dell(a,b)
+    print("Деление", delenie)"""
